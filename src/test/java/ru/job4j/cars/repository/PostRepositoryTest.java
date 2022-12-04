87c7706f09adf889b125b89b029c2348971df3b0
ValeraDanilov <delfiyets@gmail.com>
2022-12-04 14:32:02 +0200

    2. Интеграционные тесты для проект job4j_cars [#4746]

---
+++ src/test/java/ru/job4j/cars/repository/PostRepositoryTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.cars.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostRepositoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void findAllOrderById() {
+    }
+
+    @Test
+    public void findBySpecificBrand() {
+    }
+
+    @Test
+    public void findByPhoto() {
+    }
+
+    @Test
+    public void findByLastDay() {
+    }
+
+    @Test
+    public void findById() {
+    }
+}
---
+++ src/test/java/ru/job4j/cars/repository/UserRepositoryTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.cars.repository;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.cars.model.User;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class UserRepositoryTest {
+
+    private static SessionFactory sessionFactory;
+
+    private static UserRepository userRepository;
+
+    @BeforeClass
+    public static void initContext() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        userRepository = new UserRepository(new CrudRepository(sessionFactory));
+    }
+
+    @After
+    public void tearDown() {
+        var session = sessionFactory.openSession();
+        var transaction = session.beginTransaction();
+        session.createQuery("delete from User ").executeUpdate();
+        transaction.commit();
+    }
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setLogin("Ira");
+        userRepository.create(user);
+        Optional<User> findByUser = userRepository.findByLogin("Ira");
+        //  assertEquals("Ira", findByUser.get().getLogin());
+    }
+}
